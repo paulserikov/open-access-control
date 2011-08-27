d594985e74436f98b74fad6edbaeabb819ebd9c7
scottedwinbailey@gmail.com <scottedwinbailey@gmail.com@76949999-1568-b136-c756-535ca1a7980e>
2011-08-27 18:02:23 +0000

    minor prep changes:  - add svn:eol-style native to all text files, maintains line ending consistency (dos vs unix)  - added a couple files to support CMake building.  Doesn't interfere with standard IDE usage.  Just ignore them.  - removed older pde file, it's available in the repository.

---
+++ branches/library-refactor/libraries/DS1307/DS1307.h
@@ -0,0 +1,40 @@
+/* Adds support for the DS1307 real-time clock chip (RTC)
+*/
+
+#ifndef	_DS1307_H_
+#define	_DS1307_H_
+#include <stdint.h>
+
+
+#define DS1307_I2C_ADDRESS 0x68
+
+
+//extern uint8_t second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+
+class DS1307 {
+public:
+   DS1307();
+~DS1307();
+
+void setDateDs1307(uint8_t second,        // 0-59
+                   uint8_t minute,        // 0-59
+                   uint8_t hour,          // 1-23
+                   uint8_t dayOfWeek,     // 1-7
+                   uint8_t dayOfMonth,    // 1-28/29/30/31
+                   uint8_t month,         // 1-12
+                   uint8_t year);          // 0-99
+
+void getDateDs1307(uint8_t *second,
+          uint8_t *minute,
+          uint8_t *hour,
+          uint8_t *dayOfWeek,
+          uint8_t *dayOfMonth,
+          uint8_t *month,
+          uint8_t *year);
+
+private:
+uint8_t decToBcd(uint8_t val);
+uint8_t bcdToDec(uint8_t val);
+
+};
+#endif
