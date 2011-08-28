03d5728e173a6d919af9dba72de45fa4f5aee417
scottedwinbailey <scottedwinbailey@gmail.com>
2011-08-28 19:16:18 +0000

    documenatation!

--- libraries/PinCatcher/PinCatcher.h
+++ libraries/PinCatcher/PinCatcher.h
@@ -1,15 +1,29 @@
 #ifndef PINCATCHER_H
 #define PINCATCHER_H
 
+/// PinCatcher class
+/// @example Wiegang26.h
+/// @example Wiegang26.cpp
+///
+/// make sure your derived class' destructor is modified with the virtual keyword!
+///
 class PinCatcher
 {
 public:
+   /// constructor
    PinCatcher();
+   /// destructor
    virtual ~PinCatcher();
-
-   virtual void handle(unsigned pin, bool transition_high)=0;
+   /// override this function to handle the pin change
+   /// @param pin  The pin that changed
+   /// @param rising_edge_transition  true if this is a rising edge tranistion, false for falling.
+   virtual void handle(unsigned pin, bool rising_edge_transition)=0;
 protected:
+   /// attach this PinCatcher so it captures the interrupt for a given pin
+   /// @param pin  The pin to capture interrupts for
    virtual void attach(unsigned pin);
+   /// detach the PinCatcher so interrupts for a given pin are ignored
+   /// @param pin  The pin to ignore
    virtual void detach(unsigned pin);
 };
 
